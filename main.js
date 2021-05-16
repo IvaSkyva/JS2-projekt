2d38cbd0a120aff8e2a206bf2de9bb26036ef8ba
Ivana Skýva <ivana.rybakova@gmail.com>
2021-05-16 15:51:07 +0200

    tvorba funkcionality caruselu

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-const header = document.querySelector('header.header-news');
+const header = document.querySelector('div.header-news__container');
+const caruselItemCount = 4;
+let caruselItemStart = 0;
+let articles;
+
 fetch('http://localhost:3000/news.json')
     .then(serverResponse => serverResponse.text())
     .then(responseText => {
         const data = JSON.parse(responseText);
-        populateNewsCarousel(data.articles);
+        articles = data.articles;
+        populateNewsCarousel(data.articles, 0);
     });
-function populateNewsCarousel(news) {
-    for(let i = 0; i < news.length; i ++) {
+function populateNewsCarousel(news, startAt) {
+    
+    for(let i = startAt; i < (startAt + caruselItemCount); i ++) {
         const newsValue = news[i];
         const newsDiv = createDivForNews(newsValue);
         header.appendChild(newsDiv);
@@ -14,10 +20,30 @@ function populateNewsCarousel(news) {
 }
 function createDivForNews(newsContents) {
     const newsArticle = document.createElement('div');
-    newsArticle.innerText = newsContents.title;
+    
+    newsArticle.classList.add('news-article');
+    newsArticle.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), transparent), url(${newsContents.image})`;
+
+    const title = document.createElement('span');
+    title.classList.add('news-article__title');
+    title.innerText = newsContents.title;
+    newsArticle.appendChild(title);
     return newsArticle;
 }
 
+const buttonLeft = document.querySelector('#carousel-button-left');
+
+const buttonRight = document.querySelector('#carousel-button-right');
+
+buttonRight.addEventListener('click', () => {
+    caruselItemStart--;
+    populateNewsCarousel(articles, 1);
+});
+
+buttonLeft.addEventListener('click', () => {
+    caruselItemStart++;
+    populateNewsCarousel(articles, 1);
+});
 
 
 
